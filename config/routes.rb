3a106963891f4da34f6e204320aa9c6233078674
chayakornwc <whitecat.chayakorn@gmail.com>
2018-10-23 19:21:30 +0700

    :sparkles:

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  
+  get 'welcome/index'
+  get 'sessions/new'
+  resources :articles
+  root 'welcome#index'
+  
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
+
+  resources :users
+ 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
